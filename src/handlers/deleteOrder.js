3d9bd795cb944d488b87453b10adfbb1587e9087
santimazuera <santimazuera100@gmail.com>
2022-07-25 10:02:30 -0500

    add configuration table deleteorder and logic

--- src/handlers/deleteOrder.js
+++ src/handlers/deleteOrder.js
@@ -1,15 +1,25 @@
 "use strict";
 
-module.exports.handler  = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "this lambda is deleteOrder",
-        input: event,
-      },
-      null,
-      2
-    ),
+module.exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log("Body ==> ", body);
+  const table = process.env.DELETE_TABLE;
+  const dynamoDB = AWS.DynamoDB.DocumentClient();
+
+  const putParams = {
+    TableName: table,
+    Key: {
+      id: body.id,
+    },
   };
-}
+
+  try {
+    await dynamoDB.put(putParams).promise();
+  } catch (err) {
+    console.log(err);
+  }
+
+  return {
+    statusCode: 201,
+  };
+};
