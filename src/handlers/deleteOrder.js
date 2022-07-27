7667cb3fffcbc55b2d7079f3acf3cba2c2ff6e31
santimazuera <smazuera@bancolombia.com.co>
2022-07-27 10:36:47 -0500

    change deleteorder

--- src/handlers/deleteOrder.js
+++ src/handlers/deleteOrder.js
@@ -5,18 +5,49 @@ module.exports.handler = async (event, context) => {
   console.log("Body ==> ", body);
   const table = process.env.DELETE_TABLE;
   const dynamoDB = AWS.DynamoDB.DocumentClient();
-
+  let result = {};
   const putParams = {
     TableName: table,
-    Key: {
-      id: body.id,
+    Item: {
+      ...body,
     },
   };
 
   try {
     await dynamoDB.put(putParams).promise();
+    console.log("After put *+*");
+    result = {
+      statusCode: 200,
+      data: {
+        Errors: null,
+        Response: {
+          PCOrderNo: body.PCOrderNo,
+          SCOrderNo: body.PCOrderNo,
+          PCNotificationID: "0",
+          ProcessDate: "20200105",
+          ProcessTime: "230428",
+          NotificationCode: "1000",
+          NotificationDesc: "Success",
+        },
+      },
+    };
   } catch (err) {
     console.log(err);
+    result = {
+      statusCode: 200,
+      data: {
+        Errors: null,
+        Response: {
+          PCOrderNo: body.PCOrderNo,
+          SCOrderNo: body.PCOrderNo,
+          PCNotificationID: "0",
+          ProcessDate: "20200105",
+          ProcessTime: "230428",
+          NotificationCode: "6000",
+          NotificationDesc: err,
+        },
+      },
+    };
   }
 
   return {
